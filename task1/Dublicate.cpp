e41209b67938d9553d30549ad22b48338e623405
kartsen <karen.ayv12599@gmail.com>
2022-12-09 22:08:15 +0300

    init

---
+++ task1/Dublicate.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+#define forn(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main(int argc, char const *argv[])
+{
+    clock_t start = clock();
+
+    
+
+    clock_t end = clock();
+    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("The time: %f seconds\n", seconds);
+    return 0;
+}
