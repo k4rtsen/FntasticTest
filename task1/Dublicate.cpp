cd2f4d656caf67904b5e27e2f03cb09f5306c976
kartsen <karen.ayv12599@gmail.com>
2022-12-10 12:19:50 +0300

    first good result

--- task1/Dublicate.cpp
+++ task1/Dublicate.cpp
@@ -1,13 +1,48 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
 
-#define forn(i, n) for (int i = 0; i < (int)(n); i++)
+string ConvertString(string str)
+{
+    string result;
+    char dub = ')', no_dub = '(';
+    vector<char> dublicates;
+
+    for (int i = 0; i <= str.length(); i++)
+        for (int j = i + 1; j <= str.length() - 1; j++) 
+        if (str[i] == str[j])
+        {
+            dublicates.push_back(str[i]);
+            break;
+        }
+
+    for (char ch : str)
+    {
+        bool flag = false;
+        for (char val : dublicates)
+            if (ch == val) {
+                flag = true;
+                break;
+            }
+        flag ? result.push_back(dub) : result.push_back(no_dub);
+    }
+    return result;
+}
 
 int main(int argc, char const *argv[])
 {
+    string str = "(( @";
+    for_each(str.begin(),
+             str.end(),
+             [](char &c)
+             {
+                 c = ::tolower(static_cast<unsigned char>(c));
+             });
+    cout << str << endl;
+
     clock_t start = clock();
 
-    
+    str = ConvertString(str);
+    cout << str << endl;
 
     clock_t end = clock();
     double seconds = (double)(end - start) / CLOCKS_PER_SEC;
